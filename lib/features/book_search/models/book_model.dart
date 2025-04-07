cf12b02b434e1db8a5c7572588d3f47d39eef70d
Assyl <assylzfg@gmail.com>
2025-04-07 18:10:57 +0500

    Added book model

--- lib/features/book_search/models/book_model.dart
+++ lib/features/book_search/models/book_model.dart
@@ -1,3 +1,29 @@
 class BookModel {
-  
+  final String id;
+  final String title;
+  final List<String> authors;
+  final String? publishedDate;
+  final String? thumbnail;
+  final String? infoLink;
+
+  BookModel({
+    required this.id,
+    required this.title,
+    required this.authors,
+    this.publishedDate,
+    this.thumbnail,
+    this.infoLink,
+  });
+
+  factory BookModel.fromJson(Map<String, dynamic> json) {
+    final volumeInfo = json['volumeInfo'];
+    return BookModel(
+      id: json["id"],
+      title: volumeInfo["title"] ?? "No title",
+      authors: volumeInfo["authors"] ?? "Unknown",
+      publishedDate: volumeInfo["publishedDate"],
+      thumbnail: volumeInfo["imageLinks"]["thumbnail"],
+      infoLink: volumeInfo["infoLink"]
+    );
+  }
 }
